e10c29dd4833a7fe1253314c5b0eb48cc27f5e5e
petrYar <yyy1505yyy1505@gmail.com>
2020-11-10 22:26:47 +0200

    Added Controllers(Auth,Category,Product,SResult)

---
+++ ProductShop/Controllers/SingleResultDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductShop.DTO.ResultDTO;
+
+namespace ProductShop.Controllers
+{
+    public class SingleResultDTO<T> : ResultDTO
+    {
+        public T Result { get; set; }
+    }
+}
