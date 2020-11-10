6f5c450962ce3143d8806f6a28bc54412367709b
petrYar <yyy1505yyy1505@gmail.com>
2020-11-10 22:28:50 +0200

    Fixed some link-issues

--- ProductShop/Startup.cs
+++ ProductShop/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,8 +14,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using ProductShop.DAL;
 using ProductShop.DAL.Entities.Auth;
+using ProductShop.Domain.Implementation;
+using ProductShop.Domain.Interfaces;
 
 namespace ProductShop
 {
